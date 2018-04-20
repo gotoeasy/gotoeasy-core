c2a75bd78763edd91e95ecd719c043dbaf1c390c
zhangming <sunmoon2222@163.com>
2018-04-20 16:49:39 +0800

    如果需要替换日志方案，实现LoggerProvider并返回比10更小的优先度，然后以SPI方式配置就行

--- src/top/gotoeasy/framework/core/log/LoggerProvider.java
+++ src/top/gotoeasy/framework/core/log/LoggerProvider.java
@@ -6,6 +6,10 @@ package top.gotoeasy.framework.core.log;
  * 实现类需要通过SPI方式配置才能生效<br>
  * 配置参考文件 META-INF\services\top.gotoeasy.framework.core.log.LoggerProvider
  * </p>
+ * <p>
+ * 默认检查使用的顺序是：先Slf4j再log4j-api<br>
+ * 如果需要替换，实现LoggerProvider并返回比10更小的优先度，然后以SPI方式配置就行
+ * </p>
  * @since 2018/04
  * @author 青松
  */

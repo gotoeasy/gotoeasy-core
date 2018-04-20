8ac7af94f6b12e6b5ddaa232f4bf5cad0098459d
zhangming <sunmoon2222@163.com>
2018-04-20 14:09:59 +0800

    代码改善

--- src/top/gotoeasy/framework/core/log/impl/Slf4jLoggerProvider.java
+++ src/top/gotoeasy/framework/core/log/impl/Slf4jLoggerProvider.java
@@ -2,12 +2,10 @@ package top.gotoeasy.framework.core.log.impl;
 
 import java.net.URL;
 
-import top.gotoeasy.framework.core.compiler.MemoryClassLoader;
 import top.gotoeasy.framework.core.compiler.MemoryJavaCompiler;
 import top.gotoeasy.framework.core.log.Log;
 import top.gotoeasy.framework.core.log.LoggerProvider;
 import top.gotoeasy.framework.core.util.CmnClass;
-import top.gotoeasy.framework.core.util.CmnCore;
 import top.gotoeasy.framework.core.util.CmnFile;
 
 public class Slf4jLoggerProvider implements LoggerProvider {
@@ -39,14 +37,10 @@ public class Slf4jLoggerProvider implements LoggerProvider {
 			String file = Slf4jLoggerProvider.class.getPackage().getName().replace(".", "/") + "/Slf4j$Logger.txt";
 			URL url = Thread.currentThread().getContextClassLoader().getResource(file);
 			String className = Slf4jLoggerProvider.class.getPackage().getName() + ".Slf4j$Logger";
-			String src = CmnFile.readFileText(url.getPath(), "utf-8");
+			String sourceCode = CmnFile.readFileText(url.getPath(), "utf-8");
 
 			MemoryJavaCompiler compiler = new MemoryJavaCompiler();
-			compiler.compile(className, src);
-
-			MemoryClassLoader loader = new MemoryClassLoader();
-			loggerImplClass = loader.loadClass(className);
-			CmnCore.closeQuietly(loader);
+			loggerImplClass = compiler.compileAndLoadClass(className, sourceCode);
 
 			return true;
 		} catch (ClassNotFoundException e) {

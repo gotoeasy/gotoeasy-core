2262e08628d4c843fdc7e45560c69a508fb50533
zhangming <sunmoon2222@163.com>
2018-05-03 18:12:14 +0800

    恢复

--- src/main/java/top/gotoeasy/framework/core/log/Log.java
+++ src/main/java/top/gotoeasy/framework/core/log/Log.java
@@ -7,59 +7,60 @@ package top.gotoeasy.framework.core.log;
  * 在没有第三方日志环境的情况下，默认提供简易的控制台方式输出<br>
  * 在有第三方日志环境的情况下，能按优先顺序自动选择日志系统<br>
  * </p>
+ * 
  * @since 2018/04
  * @author 青松
  */
 public interface Log {
 
-	public void trace(String msg);
+    public void trace(String msg);
 
-	public void trace(String format, Object arg);
+    public void trace(String format, Object arg);
 
-	public void trace(String format, Object arg1, Object arg2);
+    public void trace(String format, Object arg1, Object arg2);
 
-	public void trace(String format, Object ... arguments);
+    public void trace(String format, Object ... arguments);
 
-	public void trace(String msg, Throwable t);
+    public void trace(String msg, Throwable t);
 
-	public void debug(String msg);
+    public void debug(String msg);
 
-	public void debug(String format, Object arg);
+    public void debug(String format, Object arg);
 
-	public void debug(String format, Object arg1, Object arg2);
+    public void debug(String format, Object arg1, Object arg2);
 
-	public void debug(String format, Object ... arguments);
+    public void debug(String format, Object ... arguments);
 
-	public void debug(String msg, Throwable t);
+    public void debug(String msg, Throwable t);
 
-	public void info(String msg);
+    public void info(String msg);
 
-	public void info(String format, Object arg);
+    public void info(String format, Object arg);
 
-	public void info(String format, Object arg1, Object arg2);
+    public void info(String format, Object arg1, Object arg2);
 
-	public void info(String format, Object ... arguments);
+    public void info(String format, Object ... arguments);
 
-	public void info(String msg, Throwable t);
+    public void info(String msg, Throwable t);
 
-	public void warn(String msg);
+    public void warn(String msg);
 
-	public void warn(String format, Object arg);
+    public void warn(String format, Object arg);
 
-	public void warn(String format, Object ... arguments);
+    public void warn(String format, Object ... arguments);
 
-	public void warn(String format, Object arg1, Object arg2);
+    public void warn(String format, Object arg1, Object arg2);
 
-	public void warn(String msg, Throwable t);
+    public void warn(String msg, Throwable t);
 
-	public void error(String msg);
+    public void error(String msg);
 
-	public void error(String format, Object arg);
+    public void error(String format, Object arg);
 
-	public void error(String format, Object arg1, Object arg2);
+    public void error(String format, Object arg1, Object arg2);
 
-	public void error(String format, Object ... arguments);
+    public void error(String format, Object ... arguments);
 
-	public void error(String msg, Throwable t);
+    public void error(String msg, Throwable t);
 
 }
--- src/main/java/top/gotoeasy/framework/core/log/impl/Slf4jLoggerProvider.java
+++ src/main/java/top/gotoeasy/framework/core/log/impl/Slf4jLoggerProvider.java
@@ -1,7 +1,13 @@
 package top.gotoeasy.framework.core.log.impl;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import top.gotoeasy.framework.core.log.Log;
 import top.gotoeasy.framework.core.log.LoggerProvider;
+import top.gotoeasy.framework.core.util.CmnClass;
+import top.gotoeasy.framework.core.util.CmnCore;
+import top.gotoeasy.framework.core.util.CmnResource;
 
 /**
  * 日志提供者 Slf4j
@@ -14,7 +20,10 @@ import top.gotoeasy.framework.core.log.LoggerProvider;
  */
 public class Slf4jLoggerProvider implements LoggerProvider {
 
-    private static final Slf4jLoggerProvider instance = new Slf4jLoggerProvider();
+    // 从Slf4j$Logger.klass装载
+    private static Class<?>                  loggerImplClass = null;
+
+    private static final Slf4jLoggerProvider instance        = new Slf4jLoggerProvider();
 
     /**
      * 取得实例
@@ -33,7 +42,7 @@ public class Slf4jLoggerProvider implements LoggerProvider {
      */
     @Override
     public Log getLogger(Class<?> clas) {
-        return new Slf4jLogger(clas);
+        return (Log)CmnClass.createInstance(loggerImplClass, new Class<?>[] {Class.class}, new Class<?>[] {clas});
     }
 
     /**
@@ -43,14 +52,24 @@ public class Slf4jLoggerProvider implements LoggerProvider {
      */
     @Override
     public boolean accept() {
+        ResourceClassLoader loader = null;
         try {
+            if ( loggerImplClass != null ) {
+                return true;
+            }
 
             Class.forName("org.slf4j.LoggerFactory");
+
+            // 装载提前编译好的Slf4j$Logger.klass，免于额外引入jar包
+            loader = new ResourceClassLoader();
+            loggerImplClass = loader.loadClass("top.gotoeasy.framework.core.log.impl.Slf4j$Logger");
 
             return true;
         } catch (Exception e) {
             System.err.println("当前环境不支持Slf4j");
             return false;
+        } finally {
+            CmnCore.closeQuietly(loader);
         }
     }
 
@@ -64,4 +83,18 @@ public class Slf4jLoggerProvider implements LoggerProvider {
         return 10;
     }
 
+    private class ResourceClassLoader extends URLClassLoader {
+
+        public ResourceClassLoader() {
+            super(new URL[0], ResourceClassLoader.class.getClassLoader());
+        }
+
+        @Override
+        protected Class<?> findClass(String className) throws ClassNotFoundException {
+            // 从同目录读取Slf4j$Logger.klass并装载
+            byte[] buf = CmnResource.getResourceBytes(className.substring(className.lastIndexOf('.') + 1) + ".klass", Slf4jLoggerProvider.class);
+            return defineClass(className, buf, 0, buf.length);
+        }
+
+    }
 }

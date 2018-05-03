410ae0335162cbe3d41274eed7177b30cc681b76
zhangming <sunmoon2222@163.com>
2018-05-03 16:43:38 +0800

    update

--- src/main/java/top/gotoeasy/framework/core/log/impl/Slf4jLoggerProvider.java
+++ src/main/java/top/gotoeasy/framework/core/log/impl/Slf4jLoggerProvider.java
@@ -1,13 +1,7 @@
 package top.gotoeasy.framework.core.log.impl;
 
-import java.net.URL;
-import java.net.URLClassLoader;
-
 import top.gotoeasy.framework.core.log.Log;
 import top.gotoeasy.framework.core.log.LoggerProvider;
-import top.gotoeasy.framework.core.util.CmnClass;
-import top.gotoeasy.framework.core.util.CmnCore;
-import top.gotoeasy.framework.core.util.CmnResource;
 
 /**
  * 日志提供者 Slf4j
@@ -20,10 +14,7 @@ import top.gotoeasy.framework.core.util.CmnResource;
  */
 public class Slf4jLoggerProvider implements LoggerProvider {
 
-    // 从Slf4j$Logger.klass装载
-    private static Class<?>                  loggerImplClass = null;
-
-    private static final Slf4jLoggerProvider instance        = new Slf4jLoggerProvider();
+    private static final Slf4jLoggerProvider instance = new Slf4jLoggerProvider();
 
     /**
      * 取得实例
@@ -42,7 +33,7 @@ public class Slf4jLoggerProvider implements LoggerProvider {
      */
     @Override
     public Log getLogger(Class<?> clas) {
-        return (Log)CmnClass.createInstance(loggerImplClass, new Class<?>[] {Class.class}, new Class<?>[] {clas});
+        return new Slf4jLogger(clas);
     }
 
     /**
@@ -52,24 +43,14 @@ public class Slf4jLoggerProvider implements LoggerProvider {
      */
     @Override
     public boolean accept() {
-        ResourceClassLoader loader = null;
         try {
-            if ( loggerImplClass != null ) {
-                return true;
-            }
 
             Class.forName("org.slf4j.LoggerFactory");
-
-            // 装载提前编译好的Slf4j$Logger.klass，免于额外引入jar包
-            loader = new ResourceClassLoader();
-            loggerImplClass = loader.loadClass("top.gotoeasy.framework.core.log.impl.Slf4j$Logger");
 
             return true;
         } catch (Exception e) {
             System.err.println("当前环境不支持Slf4j");
             return false;
-        } finally {
-            CmnCore.closeQuietly(loader);
         }
     }
 
@@ -83,18 +64,4 @@ public class Slf4jLoggerProvider implements LoggerProvider {
         return 10;
     }
 
-    private class ResourceClassLoader extends URLClassLoader {
-
-        public ResourceClassLoader() {
-            super(new URL[0], ResourceClassLoader.class.getClassLoader());
-        }
-
-        @Override
-        protected Class<?> findClass(String className) throws ClassNotFoundException {
-            // 从同目录读取Slf4j$Logger.klass并装载
-            byte[] buf = CmnResource.getResourceBytes(className.substring(className.lastIndexOf('.') + 1) + ".klass", Slf4jLoggerProvider.class);
-            return defineClass(className, buf, 0, buf.length);
-        }
-
-    }
 }

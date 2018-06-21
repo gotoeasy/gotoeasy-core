8de4a1546caf951529f21958ff6addac42c8ebeb
zhangming <sunmoon2222@163.com>
2018-06-21 15:56:36 +0800

    add getContext

--- src/main/java/top/gotoeasy/framework/core/util/CmnResource.java
+++ src/main/java/top/gotoeasy/framework/core/util/CmnResource.java
@@ -1,8 +1,11 @@
 package top.gotoeasy.framework.core.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -53,6 +56,43 @@ public class CmnResource {
             return null;
         }
         return CmnFile.readFileText(url.getPath(), "utf-8");
+    }
+
+    /**
+     * 按UTF_8编码读取指定文本文件内容
+     * 
+     * @param fileName 文件名（ 根路径下:abc.xml， 指定包路径下:com/test/abc.xml）
+     * @return 文件内容
+     */
+    public static String getContext(String fileName) {
+        return getContext(fileName, StandardCharsets.UTF_8.name());
+    }
+
+    /**
+     * 按指定编码读取指定文本文件内容
+     * 
+     * @param fileName 文件名（ 根路径下:abc.xml， 指定包路径下:com/test/abc.xml）
+     * @param charset 编码
+     * @return 文件内容
+     */
+    public static String getContext(String fileName, String charset) {
+        URL url = Thread.currentThread().getContextClassLoader().getResource(fileName);
+        if ( url == null ) {
+            log.error("文件找不到({})", fileName);
+            return null;
+        }
+
+        try ( InputStream io = url.openStream(); ByteArrayOutputStream bos = new ByteArrayOutputStream(); ) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ( (length = io.read(buffer)) != -1 ) {
+                bos.write(buffer, 0, length);
+            }
+            return bos.toString(CmnString.isNotBlank(charset) ? charset : StandardCharsets.UTF_8.name());
+        } catch (Exception e) {
+            throw new CoreException(e);
+        }
+
     }
 
     /**
